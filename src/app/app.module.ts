f77a1e776dc622b53e126fd6f929562a016c3431
T-A-S-H-U-N <ttashun.15@cse.mrt.ac.lk>
2020-07-01 22:14:44 +0530

    Proxy added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuddyMessageItemComponent } from './buddy-message-item/buddy-message-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuddyMessageItemComponent
   ],
   imports: [
     BrowserModule,
